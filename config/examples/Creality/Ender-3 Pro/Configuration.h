ca95c2d71346db32176d00c83d9ef7c48a8a1811
franco443 <57979330+franco443@users.noreply.github.com>
2021-01-30 20:00:17 -0800

    Update Configuration.h
    my z offset for bltouch and new mount

--- config/examples/Creality/Ender-3 Pro/Configuration.h
+++ config/examples/Creality/Ender-3 Pro/Configuration.h
@@ -986,7 +986,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -48, 2, -2.07 }
+#define NOZZLE_TO_PROBE_OFFSET { -23, 1, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
